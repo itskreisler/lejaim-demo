fc543a3aea2eeec51d61bc6c774c284961f6943f
Kreisler Ramirez <itskreisler@outlook.com>
2022-09-08 17:22:05 -0500

    first commit

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,7 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://itskreisler.github.io
-# git push -f git@github.com:itskreisler/itskreisler.github.io.git main
+
 
 # if you are deploying to https://itskreisler.github.io/<REPO>
 # git push -f git@github.com:itskreisler/<REPO>.git main:gh-pages
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,20 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react()],
+    base: '/lejaim-demo/',
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'app-[name].js',
+          assetFileNames: 'app-[name].css',
+          chunkFileNames: 'chunk-[name].js'
+        }
+      },
+      // Prevent vendor.css being created
+      cssCodeSplit: false,
+      // prevent some warnings
+      chunkSizeWarningLimit: 60000
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src')
